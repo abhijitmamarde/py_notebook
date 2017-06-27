933d5d134e3dd95d77bec45af60506e5dce4c602
Abhijit Mamarde <abhijit.mamarde@gmail.com>
2017-06-27 10:01:49 +0530

    updated file read sample to read the list of lines

--- programs/file_read.py
+++ programs/file_read.py
@@ -1,7 +1,21 @@
 # f = open("sample_read_file.txt")
 # f = open("./pack1/sample_read_file.txt")
-f = open("/Users/abhijitmamarde/pwork/py_notebook/programs/pack1/sample_read_file.txt")
+f = open("/Users/abhijitmamarde/pwork/py_notebook/programs/pack1/sample_read_file.txt", "r")
 print(type(f))
-data = f.read()
+
+# data = f.read()
+# print(type(data))
+# print(data)
+
+
+data = f.readlines()
 print(type(data))
 print(data)
+
+data_stripped = []
+for x in data:
+    l = x.strip()
+    if l:
+        data_stripped.append(l)
+
+print(data_stripped)
